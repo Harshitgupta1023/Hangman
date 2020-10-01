7340681a77f6ee3864f6c725fff3b7a4cb542007
Gourav Kumar <58901846+gourav2001k@users.noreply.github.com>
2020-10-01 22:11:58 +0530

    Update generate_number.py
    Removed additional code snippet which was unneccesary

--- generate_number.py
+++ generate_number.py
@@ -1,11 +1,8 @@
-# take words from a file
+# Opens a text file containing words and forms a list out of the word present in them.
 
 def words_list():
 	a = open('file.txt','r')
 	b = []
-	c = []
-	for i in a :
+	for i in a:
 		b.append(i)
-	for j in range(len(b)-1) :
-		c.append(b[j][:len(b[j])-1])
-	return (c,len(b))
+	return (b,len(b))
